b84ff11433cf15294cb05ced1c6afa82e384a0f0
Ahmed <mido.zezo545@gmail.com>
2025-01-09 15:03:19 -0800

    refactor: code in util

--- app/src/main/java/com/example/mazadytask/util/Enum.kt
+++ app/src/main/java/com/example/mazadytask/util/Enum.kt
@@ -1,18 +1,7 @@
 package com.example.mazadytask.util
 
 
-enum class DropDownId(val value: Byte) {
-    FIRST(1),
-    SECOND(2)
-}
-
-enum class MainCategory(val value: String) {
-    CARS("cars"),
-    COMPUTER("computer"),
-    MOBILE("mobile")
-}
-
-enum class RequestState(val value : String) {
+enum class RequestState(val value: String) {
     IDLE("idle"),
     SUCCESS("success"),
     ERROR("error"),
